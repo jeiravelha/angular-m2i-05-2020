0ee74965596fb24449f7f22123ac1ee454729ca5
Joao Eira <joao.eira@finelog-biseum.com>
2020-05-11 15:32:42 +0200

    Ajout des modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { IconsModule } from './icons/icons.module';
+import { LoginModule } from './login/login.module';
+import { TextModule } from './text/text.module';
+import { UiModule } from './ui/ui.module';
+
+// alt + maj + O <- mettre les imports dans l'ordre
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    CoreModule,
+    TextModule,
+    UiModule,
+    IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
